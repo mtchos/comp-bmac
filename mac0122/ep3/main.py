f594ec482885923d557bf1e27b439db7ad7cc5c2
mtchos <matheussilvaesousa@gmail.com>
2023-11-21 22:32:26 -0300

    feat: add methods

--- mac0122/ep3/main.py
+++ mac0122/ep3/main.py
@@ -1,9 +1,16 @@
+import copy
+
+
 def main():
     filename = ''
     while filename != 'fim':
         filename = input('Digite o nome do arquivo: ')
         file = open(filename)
         tab = to_tab(file)
+        tab_sort = sort(tab)
+        tab_insert = insert_sort(tab, tab_sort)
+        tab_quick = quick_sort(tab, tab_sort)
+        tab_tim = tim_sort(tab, tab_sort)
         print(tab)
 
 
@@ -13,6 +20,22 @@ def to_tab(file):
     for line in lines:
         result.append(line.split(','))
     return result
+
+
+def sort(tab):
+    return tab
+
+
+def insert_sort(tab, tab_sort):
+    return tab
+
+
+def quick_sort(tab, tab_sort):
+    return tab
+
+
+def tim_sort(tab, tab_sort):
+    return tab
 
 
 main()
