b77ecd5bc8de7e47bbe2ad0b53e44bf525710f4d
mtchos <matheussilvaesousa@gmail.com>
2023-12-10 17:21:19 -0300

    feat: implement insertion sort

--- mac0122/ep3/main.py
+++ mac0122/ep3/main.py
@@ -4,13 +4,16 @@ import copy
 def main():
     filename = ''
     while filename != 'fim':
-        filename = input('Digite o nome do arquivo: ')
+        # filename = input('Digite o nome do arquivo: ')
+        filename = 'test1.txt'
         file = open(filename)
         tab = to_tab(file)
         tab_sort = sort(tab)
-        tab_insert = insert_sort(tab, tab_sort)
+        tab_insert = insert_sort(tab)
+        print(tab_insert)
         tab_quick = quick_sort(tab, tab_sort)
         tab_tim = tim_sort(tab, tab_sort)
+        filename = 'fim'
 
 
 def to_tab(file):
@@ -24,22 +27,24 @@ def to_tab(file):
 
 def sort(tab):
     tab.sort(key=lambda x: (x[1], x[2], x[0]))
-    for line in tab:
-        print(line)
     return tab
 
 
-def insert_sort(array, tab_sort, key=None):
-    changed = True
-    while changed:
-        changed = False
-        for index, element in enumerate(array):
-            prev_element = array[index - 1]
-            if index > 0 and element[1] < prev_element[1]:
-                changed = True
-                left, right = element[1], prev_element[1]
-                array[index - 1] = left
-                array[index] = right
+def compare(entry, previous):
+    by_name = entry[1] < previous[1]
+    by_tax_id = entry[0] < previous[0] if by_name is True else False
+    by_birth_date = entry[2] < previous[2] if by_tax_id is True else False  # TODO fix date comparison
+    return by_name or by_tax_id or by_birth_date
+
+
+def insert_sort(array):
+    for index in range(1, len(array)):
+        current = array[index]
+        previous_index = index - 1
+        while previous_index >= 0 and compare(current, array[previous_index]):
+            array[previous_index + 1] = array[previous_index]
+            previous_index -= 1
+        array[previous_index + 1] = current
     return array
 
 
